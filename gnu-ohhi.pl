0c99f21ab4949765a5c416167030132dec4d3606
Spencer Gouw <swyverng55@g.ucla.edu>
2023-06-28 17:41:55 -0700

    feat: gnu solver reads cmdline args

--- gnu-ohhi.pl
+++ gnu-ohhi.pl
@@ -4,13 +4,13 @@ fliplength(N, T) :- length(T, N).
 
 foldl(_, [], [], Result, Result).
 foldl(Goal, [H1|T1], [H2|T2], Init, Result) :-
-	call(Goal, H1, H2, Init, InitPrime),
-	foldl(Goal, T1, T2, InitPrime, Result).
+    call(Goal, H1, H2, Init, InitPrime),
+    foldl(Goal, T1, T2, InitPrime, Result).
 transpose([], []).
 transpose([L|Ls], Ts) :-
     foldl(transpose_helper, L, Ts, [L|Ls], _).
 transpose_helper(_, HeadsList, Matrix, TailsList) :-
-	maplist(heads_tails, Matrix, HeadsList, TailsList).
+    maplist(heads_tails, Matrix, HeadsList, TailsList).
 heads_tails([L|Ls], L, Ls).
 
 %% Binairo/Takuzu/0hh1/whatever.
@@ -35,7 +35,7 @@ list_member(Xs, [Hs|Ts]) :- list_eq(Xs, Hs) ; list_member(Xs, Ts).
 % no_dup_lists([]).
 % no_dup_lists([Xs|Xss]) :- not(list_member(Xs, Xss)), no_dup_lists(Xss).
 
-% A little faster.  Since we're not using them on CLPB variables anyway.
+% A little faster.  Since we are not using them on CLPB variables anyway.
 no_dups_labeled([]).
 no_dups_labeled([Xs|Xss]) :-
     list_member(Xs, Xss), !, fail;
@@ -86,7 +86,20 @@ solve_test(N0, C, T, R) :-
 
 solve_spec :- ohhi(6, [c(1, [1|1]),c(1, [1|4]),c(0, [1|7]),c(1, [2|3]),c(0, [2|10]),c(1, [3|3]),c(0, [3|5]),c(1, [3|8]),c(0, [3|10]),c(0, [3|12]),c(1, [4|1]),c(1, [4|2]),c(0, [4|5]),c(1, [5|2]),c(0, [5|6]),c(1, [5|8]),c(1, [5|10]),c(0, [6|6]),c(0, [6|9]),c(0, [7|3]),c(1, [8|10]),c(1, [8|11]),c(0, [9|1]),c(0, [9|2]),c(1, [9|4]),c(0, [9|6]),c(0, [9|9]),c(1, [10|6]),c(1, [10|7]),c(1, [10|12]),c(1, [11|4]),c(0, [11|9]),c(0, [12|3]),c(0, [12|6]),c(0, [12|12])]).
 
-:- initialization(solve_spec).
+% gprolog gets command-line input from argument_value/2. Look it up in the manual.
+% Use in tandem with shell expansion.
+main :-
+    % Set double-quoted things as atoms instead of strings (character lists).
+    set_prolog_flag(double_quotes, atom),
+    argument_value(1, N0_STR), number_atom(N0, N0_STR), % Converts string to number (parse)
+    % Apparently, atom (strings) end with eof. Crazy.
+    argument_value(2, C_STR), read_term_from_atom(C_STR, C, [end_of_term(eof)]), % is what is used to convert shell-given strings to terms, see http://www.gprolog.org/manual/gprolog.html#sec81
+    % write(N0), nl,
+    % write(C), nl,
+    ohhi(N0, C),
+    halt.
+
+:- initialization(main).
 
 % So, I tried to use the solver on a 12x12 board, and my fastest 12x12 time is 5:40... I was 75% done with the inputs when I realized I had taken 7 minutes already... crime doesn't pay.
 % Welp.  Just got to automate the input too!
