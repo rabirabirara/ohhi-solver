4384575b319eb93506adf280d8dba47f99f776da
Spencer Gouw <swyverng55@g.ucla.edu>
2023-07-12 09:14:16 -0700

    wip: square detection (contours)

--- detect.py
+++ detect.py
@@ -1,23 +1,27 @@
 import cv2 as cv
 import numpy as np
 import argparse
+from math import sqrt
 
 def show(img):
-    cv.imshow('', img)
-    if cv.waitKey(0) == ord('q'):
+    cv.imshow("", img)
+    if cv.waitKey(0) == ord("q"):
         cv.destroyAllWindows()
+
 
 def d():
     cv.destroyAllWindows()
-    
+
+
 def get_edges(img):
     return cv.Canny(img, 100, 200)
-  
+
+
 # Parse cmd-line args
-parser = argparse.ArgumentParser(description='Process an image.')
-parser.add_argument('image', type=str, help='Name of the image file.')
+parser = argparse.ArgumentParser(description="Process an image.")
+parser.add_argument("image", type=str, help="Name of the image file.")
 args = parser.parse_args()
-image = args.image    
+image = args.image
 
 # Use OpenCV to read the image
 img = cv.imread(image)
@@ -27,8 +31,8 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 # Do away with bg color
 # * This lower range is because of some #212121 line in theme 1... probably in the other themes too
-gray_lower = np.array([33,33,33])
-gray_upper = np.array([35,35,35])
+gray_lower = np.array([33, 33, 33])
+gray_upper = np.array([35, 35, 35])
 gray_mask = cv.bitwise_not(cv.inRange(img, gray_lower, gray_upper))
 no_bg = cv.bitwise_and(img, img, mask=gray_mask)
 
@@ -42,5 +46,41 @@ no_bg = cv.bitwise_and(img, img, mask=gray_mask)
 # Edge detection on colored image will yield the bottom bezel lines, this can be used to detect where the filled squares are.
 edges = get_edges(no_bg)
 
+# Find contours
+contours, hierarchy = cv.findContours(edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+board_contours = []
+
+# Filter contours by area, shape, aspect_ratio, hierarchy
+for contour in contours:
+    area = cv.contourArea(contour)
+    # perimeter = cv.arcLength(contour, True)
+    if area == 0.0 or area < 500:
+        continue
+    # Perimeter-Area relation: https://link.springer.com/chapter/10.1007/978-1-4899-2124-6_12
+    # ratio = perimeter / sqrt(area)
+    # ratio should be around 4 for a square
+    # if not 3.8 < ratio < 4.1:
+        # pass (see below)
+    x, y, w, h = cv.boundingRect(contour)
+    aspect_ratio = float(w) / h
+    if not 0.9 < aspect_ratio < 1.1:
+        print(f"not a square: {aspect_ratio}")
+        cv.drawContours(img, [contour], 0, (255,0,0), 2)
+        continue
+    
+    cv.drawContours(img, [contour], 0, (0,255,0), 2)
+    board_contours.append(contour)
+
+board = cv.drawContours(img, board_contours, -1, (0,255,0), 2)
+# weird = cv.drawContours(img, weird_contours, -1, (0,255,0), 2)
+    
+    
+        
+    
+
+
+# cv.drawContours(img, contours, -1, (0,255,0), 3)
+
 # if __name__ == '__main__':
-    # main()
+# main()
